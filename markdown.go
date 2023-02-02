972de75024794593a7a4316e73951f8f9377d6a0
liyou <liyou1202@gmail.com>
2023-02-03 01:37:25 +0800

    調整 輸出

--- markdown.go
+++ markdown.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"strings"
 )
 
 func main() {
+	var exportPath string
+	for i, arg := range os.Args[1:] {
+
+		if i >= 2 {
+			fmt.Println("accept only one argument file Path, e.g. /path/to/export/doc.md")
+			return
+		}
+		exportPath = arg
+	}
+
 	d := NewDoc()
 	d.AddTitle("test", 3).
+		AddBlankLines(2).
 		AddTitle("t*es#t2", 1)
-	fmt.Println(d.content)
+
+	err := d.Export(exportPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 type markdownDoc struct {
@@ -35,6 +53,20 @@ func (doc *markdownDoc) AddTitle(t string, lv int) *markdownDoc {
 	mdSyntax := strings.Repeat("#", lv) + " " + t
 	doc.writeLine(mdSyntax)
 	return doc
+}
+
+func (doc *markdownDoc) AddBlankLines(lv int) *markdownDoc {
+	if lv > 0 {
+		for i := 1; i <= lv; i++ {
+			doc.writeLine("")
+		}
+	}
+
+	return doc
+}
+
+func (doc *markdownDoc) Export(filename string) error {
+	return ioutil.WriteFile(filename, []byte(doc.content.String()), os.ModePerm)
 }
 
 func replaceEscapeCharacter(content string) string {
